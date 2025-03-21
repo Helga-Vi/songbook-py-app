6e8669b7bb505e7c1b4bb4b4840b40c950e8864b
Helga-Vi <katarina.jorgensen@gmail.com>
2025-03-21 20:51:49 +0100

    inserting input into database

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         message = request.form.get('message')
+
+         # Connect to MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['songbook_db']  # Create a database named 'songbook_db'
+        collection = db['messages']  # Create a collection named 'messages'
+
+        # Insert the message into MongoDB
+        result = collection.insert_one({'text': message})
+        print(f"Inserted document with ID: {result.inserted_id}")
+
         return render_template('index.html', message=message)
     return render_template('index.html')
 
