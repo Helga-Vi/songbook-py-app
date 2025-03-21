c04aec59edc5761dcfbad80fccaa2e38360155f8
Helga-Vi <katarina.jorgensen@gmail.com>
2025-03-21 22:32:26 +0100

    include artist in query

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ import random
 app = Flask(__name__)
 
 # Connect to MongoDB
-client = MongoClient('mongdb://localhost:27017/')
+client = MongoClient('mongodb://localhost:27017/')
 db = client['songbook_db']  # Create a database named 'songbook_db'
 collection = db['messages']  # Create a collection named 'messages'
 
 @app.route('/')
 def home():
     # Get a list of all songs
-    all_songs = list(collection.find({}, {'title': 1, '_id': 0}))
+    all_songs = list(collection.find({}, {'Sangtittel': 1, 'Artist':1, '_id': 0}))
     
     # Randomly select a song
     random_song = random.choice(all_songs)
     
     return render_template('index.html', 
-                          title=random_song['title'],
-                          artist=random_song['title'].split()[0])
+                          title=random_song['Sangtittel'],
+                          artist=random_song['Artist'].split()[0])
 
 @app.route('/process_request', methods=['POST'])
 def process_request():
