b0317888c5f1e47beb068643e7f3c1b55643f85b
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-05 16:34:42 +0200

    adding back old home route

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -5,4 +5,18 @@ main_routes = Blueprint('main', __name__)
 
 @main_routes.route('/')
 def home():
-    return jsonify({"ok": True})
+    def home():
+    # Get a list of all songs
+    all_songs = list(collection.find({}, {'Sangtittel': 1, 'Artist':1, 'Spraak':1,'_id': 0}))
+    
+    # Randomly select a song
+    random_song = random.choice(all_songs)
+
+    # Store the language in session
+   # session['song_language'] = random_song['Spraak']
+    
+    return render_template('index.html', 
+                          title=random_song['Sangtittel'],
+                          artist=random_song['Artist'],
+                          language=random_song['Spraak'])
+
