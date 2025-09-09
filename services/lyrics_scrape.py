e19fa59e7056bdd98b2d1997612341c498761164
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-09 15:22:04 +0200

    making a function for scraping

--- services/lyrics_scrape.py
+++ services/lyrics_scrape.py
@@ -4,6 +4,8 @@ import json
 
 #Henter websiden, parser html-kodene og finner tekstdelen av den. Henter også rett encoding for å skrive til fil.
 #Encoding er tilpasset alfabetet for språkene f.eks. utf-8. Særlig viktig når æøå brukes.
+def scrape_lyrics(http, song_title, artist):
+
 html = requests.get(gsearch_list[link-1])
 lang_encoding = html.encoding
 soup = BeautifulSoup(html.text, 'html.parser')
