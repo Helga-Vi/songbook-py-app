59adf165b86c795f2946a7b9cc480ec688b8e818
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-09 15:18:51 +0200

    making a script for scraping lyrics

---
+++ services/lyrics_scrape.py
@@ -0,0 +1,15 @@
+#Henter websiden, parser html-kodene og finner tekstdelen av den. Henter også rett encoding for å skrive til fil.
+#Encoding er tilpasset alfabetet for språkene f.eks. utf-8. Særlig viktig når æøå brukes.
+html = requests.get(gsearch_list[link-1])
+lang_encoding = html.encoding
+soup = BeautifulSoup(html.text, 'html.parser')
+song_text = soup.get_text()
+
+#Skriver ut sangteksten til fil:
+filename = search_string + " " + lang
+text_file = open(filename+".txt", "w", encoding = lang_encoding)
+text_file.write(song_text)
+text_file.close()
+        
+print(f"\nSjekk tekstfilen på C:/brukere/dittnavn. Den heter {filename}")
+        
