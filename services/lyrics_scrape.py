1966e8fc88a3f29ac55e51bbd6cd2cc697bfa814
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-13 23:38:39 +0200

    Function needs more variables

--- services/lyrics_scrape.py
+++ services/lyrics_scrape.py
@@ -4,7 +4,7 @@ import json
 
 #Henter websiden, parser html-kodene og finner tekstdelen av den. Henter også rett encoding for å skrive til fil.
 #Encoding er tilpasset alfabetet for språkene f.eks. utf-8. Særlig viktig når æøå brukes.
-def scrape_lyrics(choice, gsearch_list):
+def scrape_lyrics(choice, gsearch_list, search_string, lang):
 
     html = requests.get(gsearch_list[choice-1])
     lang_encoding = html.encoding
@@ -12,6 +12,7 @@ def scrape_lyrics(choice, gsearch_list):
     song_text = soup.get_text()
 
     #Skriver ut sangteksten til fil:
+    #Filnavn mismatch skylles flytting av kode
     filename = search_string + " " + lang
     text_file = open(filename+".txt", "w", encoding = lang_encoding)
     text_file.write(song_text)
