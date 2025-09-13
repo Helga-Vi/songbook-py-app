8d3ca4876b302ee9b1a1f79910f758121cc6adc7
Helga-Vi <katarina.jorgensen@gmail.com>
2025-09-13 23:36:55 +0200

    new input variables

--- services/lyrics_scrape.py
+++ services/lyrics_scrape.py
@@ -4,18 +4,18 @@ import json
 
 #Henter websiden, parser html-kodene og finner tekstdelen av den. Henter også rett encoding for å skrive til fil.
 #Encoding er tilpasset alfabetet for språkene f.eks. utf-8. Særlig viktig når æøå brukes.
-def scrape_lyrics(http, song_title, artist):
+def scrape_lyrics(choice, gsearch_list):
 
-html = requests.get(gsearch_list[link-1])
-lang_encoding = html.encoding
-soup = BeautifulSoup(html.text, 'html.parser')
-song_text = soup.get_text()
+    html = requests.get(gsearch_list[choice-1])
+    lang_encoding = html.encoding
+    soup = BeautifulSoup(html.text, 'html.parser')
+    song_text = soup.get_text()
 
-#Skriver ut sangteksten til fil:
-filename = search_string + " " + lang
-text_file = open(filename+".txt", "w", encoding = lang_encoding)
-text_file.write(song_text)
-text_file.close()
+    #Skriver ut sangteksten til fil:
+    filename = search_string + " " + lang
+    text_file = open(filename+".txt", "w", encoding = lang_encoding)
+    text_file.write(song_text)
+    text_file.close()
         
-print(f"\nSjekk tekstfilen på C:/brukere/dittnavn. Den heter {filename}")
+    print(f"\nSjekk tekstfilen på C:/brukere/dittnavn. Den heter {filename}")
         
